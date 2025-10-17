2a365e2fa750bbeaa0e320cc08179fa0854c3daa
anitaprova <104943181+anitaprova@users.noreply.github.com>
2025-10-17 18:08:40 -0400

    pushing changes

---
+++ my-react-app/src/components/Navbar.tsx
@@ -0,0 +1,13 @@
+function Navbar() {
+  return (
+    <>
+      <nav className="bg-fushia text-xl flex text-white justify-around p-3 rounded-full ml-25 mr-25 mt-5">
+        <div className="flex items-center gap-2 hover:cursor-pointer hover:animate-bounce">
+          <span>Home</span>
+        </div>
+      </nav>
+    </>
+  );
+}
+
+export default Navbar;
