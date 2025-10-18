5206a42ae185101ad8f777b1f7d7a55bacae3ada
Michelle <michelleh11219@gmail.com>
2025-10-18 19:40:34 -0400

    Updated: nav bar styling

--- my-react-app/src/components/Navbar.tsx
+++ my-react-app/src/components/Navbar.tsx
@@ -1,33 +1,35 @@
 import { useNavigate } from "react-router-dom";
 import { MdOutlineAccountCircle } from "react-icons/md";
-import ReactLogo from "../assets/logo.svg";
+import Logo from "../assets/logo.svg";
 
 function Navbar() {
   const navigate = useNavigate();
 
   return (
-    <nav className="bg-white/[0.6] font-sans shadow-[0px_1px_5px_0px_rgba(0,_0,_0,_0.25)] text-[36px] justify-between flex flex-row gap-x-10 font-sans text-xl justify-around p-3 rounded-md mx-25 mt-10">
-      <div>
-        <div
-          className="flex items-center gap-2 hover:cursor-pointer"
-          onClick={() => navigate(`/`)}
-        >
-          <img src={ReactLogo} alt="React Logo" onClick={() => navigate(`/`)} />
-          <p>
-            Med<span className="text-[#019A6A]">Speak</span>
-          </p>
-        </div>
+    <nav className="bg-white/[0.6] font-sans shadow-[0px_1px_5px_0px_rgba(0,_0,_0,_0.25)] text-xl flex flex-row justify-between px-10 p-3 rounded-md mx-25 mt-5">
+      <div
+        className="flex items-center gap-2 group cursor-pointer"
+        onClick={() => navigate(`/`)}
+      >
+        <img 
+        src={Logo} 
+        alt="MedSpeak Logo" onClick={() => navigate(`/`)} 
+        className="group-hover:hue-rotate-30 transition-all duration-300"
+        />
+        <p>
+          Med<span className="text-[#019A6A] group-hover:text-darkblue transition-all duration-300">Speak</span>
+        </p>
       </div>
 
       <div className="flex flex-row gap-5">
         <div
-          className="flex items-center gap-2 hover:cursor-pointer"
+          className="flex items-center gap-2 hover:cursor-pointer hover:text-darkblue transition-all duration-300"
           onClick={() => navigate(`/Records`)}
         >
           <span>Records</span>
         </div>
         <div
-          className="flex items-center gap-2 hover:cursor-pointer"
+          className="flex items-center gap-2 hover:cursor-pointer hover:text-darkblue transition-all duration-300"
           onClick={() => navigate(`/Dictionary`)}
         >
           <span>Dictionary</span>
@@ -36,7 +38,7 @@ function Navbar() {
           className="flex items-center gap-2 hover:cursor-pointer"
           onClick={() => navigate(`/Account`)}
         >
-          <MdOutlineAccountCircle size={45} />
+          <MdOutlineAccountCircle size={45} className="hover:text-darkblue transition-all duration-300" />
         </div>
       </div>
     </nav>
