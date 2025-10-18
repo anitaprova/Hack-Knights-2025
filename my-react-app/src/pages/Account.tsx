4fa9d5fdecd9354e2ecaf9e3d1a693e50114bc4a
Michelle <michelleh11219@gmail.com>
2025-10-18 15:37:17 -0400

    Added: logout

--- my-react-app/src/pages/Account.tsx
+++ my-react-app/src/pages/Account.tsx
@@ -1,7 +1,18 @@
+import { useNavigate } from "react-router";
+import { supabase } from "../../utils/supabaseClient";
+
 function Account() {
+  const navigate = useNavigate();
+  const signOut = async () => {
+    await supabase.auth.signOut();
+    navigate("/Login");
+  }
   return (
     <div>
-      <h1>Accounts</h1>
+      <h1>Account</h1>
+      <button onClick={signOut}>
+        Logout
+      </button>
     </div>
   );
 }
