7db04e1094ee1c7abccbb87a4c345abc2cf377b9
anitaprova <104943181+anitaprova@users.noreply.github.com>
2025-10-18 18:34:52 -0400

    finished dictionary

--- my-react-app/src/pages/Results.tsx
+++ my-react-app/src/pages/Results.tsx
@@ -47,10 +47,8 @@ function Results() {
     }
   };
 
-	console.log(promptResponses);
-
   return (
-    <div className="flex flex-row font-sans font-sans text-xl justify-around p-3 rounded-lg mx-75 mt-15">
+    <div className="flex flex-row font-sans text-xl justify-around p-3 rounded-lg mx-75 mt-15">
       {!loading ? (
         <div className="w-full bg-gray-200 flex flex-col gap-y-3 p-3 rounded-md">
           <h1 className="text-center text-[36px] font-bold">Model Output</h1>
