911e5de62f62e08a7f2972e8111a6e7a8263b9ce
Michelle <michelleh11219@gmail.com>
2025-10-17 22:33:41 -0400

    Added: supabase client

--- my-react-app/src/App.tsx
+++ my-react-app/src/App.tsx
@@ -1,12 +1,12 @@
-import { useState } from "react";
+import { useState } from 'react'
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import Navbar from "./components/Navbar";
 import "./App.css";
+import { supabase } from "../utils/supabaseClient";
 
 function App() {
   const [count, setCount] = useState(0);
-
   return (
     <>
       <Navbar />
