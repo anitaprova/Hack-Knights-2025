2aff5ca9ec00196944ab3ac2deee54277597722f
anitaprova <104943181+anitaprova@users.noreply.github.com>
2025-10-19 00:27:37 -0400

    redid home page

--- my-react-app/src/pages/Home.tsx
+++ my-react-app/src/pages/Home.tsx
@@ -1,8 +1,11 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import ReactMarkdown from "react-markdown";
+import CircularProgress from "@mui/material/CircularProgress";
 import { FaMicrophone } from "react-icons/fa";
 import { FaCloudArrowUp } from "react-icons/fa6";
 import { FaStop } from "react-icons/fa";
+import { GoogleGenerativeAI } from "@google/generative-ai";
 import SpeechRecognition, {
   useSpeechRecognition,
 } from "react-speech-recognition";
@@ -15,19 +18,53 @@ function Home() {
   const [userInput, setUserInput] = useState("");
   const [files, setFiles] = useState<File[]>([]);
   const [fileText, setFileText] = useState("");
-  const {
-    transcript,
-    listening,
-    resetTranscript,
-    browserSupportsSpeechRecognition,
-  } = useSpeechRecognition();
-  const [value, setValue] = useState(0);
-  const handleChange = (event, newValue) => {
-    setValue(newValue);
+  const { transcript, listening, browserSupportsSpeechRecognition } =
+    useSpeechRecognition();
+
+  const [textValue, setTextValue] = useState(0);
+  const [resultValue, setResultValue] = useState(0);
+  const [loading, setLoading] = useState(false);
+  const [promptResponses, setpromptResponses] = useState([""]);
+  const [type, setType] = useState("");
+  const genAI = new GoogleGenerativeAI(import.meta.env.VITE_GEMINI_API_KEY);
+
+  const getResponseForGivenPrompt = async (inputType: string) => {
+    let prompt = "";
+    if (inputType === "transcript") {
+      prompt = `You are helping in an application that translates medical speech to plain english. You are given a transcript of what the doctor has said to a patient. Return only your response. Translate the following: ${transcript}`;
+    } else if (inputType === "userInput") {
+      prompt = `You are helping in an application that translates medical speech to plain english. You are given the notes the doctor has given to the patient. Return only your response. Translate the following: ${userInput}`;
+    } else if (inputType === "file") {
+      prompt = `You are helping in an application that translates medical speech to plain english. You are given the text from a file that the doctor gave to the patient. Return only your response. Translate the following: ${fileText}`;
+    } else {
+      console.log("No passed prompt");
+      return;
+    }
+
+    try {
+      const model = genAI.getGenerativeModel({ model: "gemini-2.5-pro" });
+      setLoading(true);
+      const result = await model.generateContent(prompt);
+      const response = result.response;
+      const text = response.text();
+      setpromptResponses([text]);
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+      setLoading(false);
+    }
   };
 
+  const handleSubmit = (text: string) => {
+    setType(text);
+    getResponseForGivenPrompt(text);
+  };
+
+  const handleTextChange = (event, newValue) => setTextValue(newValue);
+  const handleResultChange = (event, newValue) => setResultValue(newValue);
+
   if (!browserSupportsSpeechRecognition) {
-    console.log("No support");
+    console.log("Speech recognition not supported in this browser.");
   }
 
   const handleFileChange = (event) => {
@@ -48,77 +85,15 @@ function Home() {
     setUserInput(e.target.value);
   };
 
-  console.log(listening);
-
   return (
-    <div className="grid grid-cols-2 gap-x-20 ml-50 mr-50 mb-5">
-      <div className="bg-lightblue rounded-xl flex flex-col text-xl mt-20 w-full shadow-custom">
-        <h1 className="flex text-xl gap-x-2 mb-2 p-8 justify-center">
-          <p className="text-center text-4xl font-bold">Audio Recording</p>
-        </h1>
-
-        <div className="p-3 flex-1">
-          {listening ? (
-            <div className="flex items-center justify-center">
-              <div className="flex flex-col gap-y-5 items-center">
-                <button
-                  onClick={SpeechRecognition.startListening}
-                  className="bg-red-200 p-6 rounded-full w-fit"
-                >
-                  <FaStop size={100} />
-                </button>
-                <p className="text-[24px]">Click to stop recording</p>
-              </div>
-            </div>
-          ) : (
-            <div className="flex items-center justify-center">
-              <div className="flex flex-col gap-y-5 items-center">
-                <button
-                  onClick={SpeechRecognition.startListening}
-                  className="bg-blue p-6 rounded-full w-fit"
-                >
-                  <FaMicrophone size={100} />
-                </button>
-
-                <p className="text-[24px]">Click to start recording</p>
-              </div>
-            </div>
-          )}
-
-          {/* <button
-              onClick={SpeechRecognition.stopListening}
-              className="bg-red-200 p-2 rounded-xl"
-            >
-              Stop
-            </button>
-            <button
-              onClick={resetTranscript}
-              className="bg-blue-200 p-2 rounded-xl mt-auto"
-            >
-              Reset
-            </button> */}
-        </div>
-
-        <div className="p-3 w-full h-full mb-5">
-          <p className="w-full h-full bg-gray-50 border border-blue text-gray-900 text-sm rounded-xl p-8 overflow-scroll [&::-webkit-scrollbar]:hidden [-ms-overflow-style:none] [scrollbar-width:none]">
-            {transcript}
-          </p>
-        </div>
-        <Link
-          to="/Results"
-          state={{ transcript }}
-          className="bg-darkblue text-4xl rounded-b-lg p-2 text-white hover:cursor-pointer text-center"
-        >
-          Translate
-        </Link>
-      </div>
-
-      <div className="bg-lightgreen shadow-custom rounded-xl flex flex-col text-xl mt-20 w-full text-center">
-        <h1 className="text-4xl text p-5 font-bold">Text Document</h1>
+    <div className="grid grid-cols-2 gap-x-20 h-[75vh] mx-25 mt-10 m-10">
+      {/* Add Record */}
+      <div className="bg-lightgreen shadow-custom rounded-xl flex flex-col text-xl text-center overflow-y-auto [&::-webkit-scrollbar]:hidden [-ms-overflow-style:none] [scrollbar-width:none]">
+        <h1 className="text-4xl p-6 font-bold">Add Record</h1>
         <div className="flex justify-center">
           <Tabs
-            value={value}
-            onChange={handleChange}
+            value={textValue}
+            onChange={handleTextChange}
             className="bg-green w-fit rounded-lg text-black"
             TabIndicatorProps={{
               style: { backgroundColor: "#5AA057" },
@@ -135,9 +110,7 @@ function Home() {
                   border: "2px solid green",
                   borderRadius: "8px 0px 0px 8px",
                 },
-                "&:hover": {
-                  backgroundColor: "#e0f9e2ff",
-                },
+                "&:hover": { backgroundColor: "#e0f9e2ff" },
               }}
             />
             <Tab
@@ -149,19 +122,32 @@ function Home() {
                   backgroundColor: "#ffffffff",
                   padding: "15px",
                   border: "2px solid green",
+                  borderRadius: "0px 0px 0px 0px",
+                },
+                "&:hover": { backgroundColor: "#e0f9e2ff" },
+              }}
+            />
+            <Tab
+              label="Audio"
+              sx={{
+                "&.Mui-selected": {
+                  color: "#5AA057",
+                  fontWeight: "bold",
+                  backgroundColor: "#ffffffff",
+                  padding: "15px",
+                  border: "2px solid green",
                   borderRadius: "0px 8px 8px 0px",
                 },
-                "&:hover": {
-                  backgroundColor: "#e0f9e2ff",
-                },
+                "&:hover": { backgroundColor: "#e0f9e2ff" },
               }}
             />
           </Tabs>
         </div>
 
-        {value === 0 && (
-          <div className="rounded-xl flex flex-1 flex-col text-xl mt-5 w-full">
-            <div className="h-full">
+        {/* Upload File Tab */}
+        {textValue === 0 && (
+          <div className="rounded-xl flex flex-col text-xl w-full flex-1 h-full">
+            <div className="flex flex-1 flex-col items-center justify-center">
               <input
                 type="file"
                 accept=".pdf"
@@ -170,54 +156,176 @@ function Home() {
                 hidden
               />
               <label htmlFor="fileUpload">
-                <div className="border border-dashed border-5 border-darkgreen p-3 m-10 text-center hover:cursor-pointer">
+                <div className="border-dashed border-5 border-darkgreen p-4 text-center hover:cursor-pointer">
                   <div className="flex justify-center">
                     <FaCloudArrowUp size={100} />
                   </div>
-                  <p className="text-[24px]">Browse file to upload</p>
-                  <p className="text-[15px]">Accepted file types: PDF</p>
+                  <p className="text-2xl">Browse file to upload</p>
+                  <p className="text-sm">Accepted file types: PDF</p>
                 </div>
               </label>
 
               {fileText ? (
-                <div className="bg-gray-50 flex gap-x-2 p-1.5 rounded-sm">
+                <div className="bg-green-300/[0.35] m-5 flex gap-x-2 p-1.5 rounded-sm">
                   <IoDocumentText size={25} />
                   <p>{files?.name}</p>
                 </div>
-              ) : (
-                <p></p>
-              )}
+              ) : null}
             </div>
 
-            <Link
-              to="/Results"
-              state={{ fileText }}
-              className="bg-darkgreen text-4xl rounded-b-lg p-2 text-white hover:cursor-pointer text-center"
+            {/* Button pinned to bottom */}
+            <button
+              className="bg-darkgreen text-4xl rounded-b-lg p-2 text-white hover:cursor-pointer text-center mt-auto"
+              onClick={() => handleSubmit("file")}
             >
               Translate
-            </Link>
+            </button>
           </div>
         )}
 
-        {value === 1 && (
+        {/* Text Editor Tab */}
+        {textValue === 1 && (
           <div className="flex flex-col flex-1 gap-1 h-full w-full">
             <div className="flex flex-col flex-1 m-10">
               <p className="text-left pb-2 pt-4">Write your medical notes</p>
               <textarea
                 value={userInput}
                 onChange={handleUserInput}
-                className="w-full h-full bg-background rounded-xl border border-darkgreen text-gray-900 text-sm rounded-sm p-3 resize-none overflow-scroll [&::-webkit-scrollbar]:hidden [-ms-overflow-style:none] [scrollbar-width:none]"
+                className="w-full h-full bg-background rounded-xl border border-darkgreen text-gray-900 text-sm p-3 resize-none overflow-y-auto [&::-webkit-scrollbar]:hidden [-ms-overflow-style:none] [scrollbar-width:none]"
               />
             </div>
-            <Link
-              to="/Results"
-              state={{ userInput }}
+
+            <button
               className="bg-darkgreen text-4xl rounded-b-lg p-2 text-white hover:cursor-pointer text-center"
+              onClick={() => handleSubmit("userInput")}
             >
               Translate
-            </Link>
+            </button>
           </div>
         )}
+
+        {/* Audio Tab */}
+        {textValue === 2 && (
+          <div className="flex flex-col h-full w-full flex-1 mt-5">
+            {listening ? (
+              <div className="flex items-center justify-center flex-1">
+                <div className="flex flex-col gap-y-5 items-center">
+                  <button
+                    onClick={SpeechRecognition.stopListening}
+                    className="bg-red-200 p-6 rounded-full w-fit mb-5 hover:cursor-pointer"
+                  >
+                    <FaStop size={100} />
+                  </button>
+                  <p className="text-3xl mt-5">Start Recording</p>
+                </div>
+              </div>
+            ) : (
+              <div className="flex items-center justify-center flex-1">
+                <div className="flex flex-col items-center">
+                  <button
+                    onClick={SpeechRecognition.startListening}
+                    className="bg-green p-6 rounded-full w-fit mb-5 hover:cursor-pointer"
+                  >
+                    <FaMicrophone size={100} />
+                  </button>
+                  <p className="text-3xl mt-5">Start Recording</p>
+                </div>
+              </div>
+            )}
+
+            {/* Push button to bottom */}
+            <button
+              className="bg-darkgreen text-4xl rounded-b-lg p-2 text-white hover:cursor-pointer text-center mt-auto"
+              onClick={() => handleSubmit("transcript")}
+            >
+              Translate
+            </button>
+          </div>
+        )}
+      </div>
+
+      {/* Results */}
+      <div className="bg-gray-100 rounded-xl shadow-custom flex flex-col overflow-y-auto [&::-webkit-scrollbar]:hidden [-ms-overflow-style:none] [scrollbar-width:none]">
+        <h1 className="text-center text-4xl font-bold m-5">Results</h1>
+        <div className="flex justify-center">
+          <Tabs
+            value={resultValue}
+            onChange={handleResultChange}
+            className="bg-green w-fit rounded-lg text-black"
+            TabIndicatorProps={{
+              style: { backgroundColor: "#5AA057" },
+            }}
+          >
+            <Tab
+              label="Transcription"
+              sx={{
+                "&.Mui-selected": {
+                  color: "#5AA057",
+                  fontWeight: "bold",
+                  backgroundColor: "#ffffffff",
+                  padding: "15px",
+                  border: "2px solid green",
+                  borderRadius: "8px 0px 0px 8px",
+                },
+                "&:hover": { backgroundColor: "#e0f9e2ff" },
+              }}
+            />
+            <Tab
+              label="Results"
+              sx={{
+                "&.Mui-selected": {
+                  color: "#5AA057",
+                  fontWeight: "bold",
+                  backgroundColor: "#ffffffff",
+                  padding: "15px",
+                  border: "2px solid green",
+                  borderRadius: "0px 8px 8px 0px",
+                },
+                "&:hover": { backgroundColor: "#e0f9e2ff" },
+              }}
+            />
+          </Tabs>
+        </div>
+
+        <div className="flex-1 overflow-y-scroll px-5 pb-5 [&::-webkit-scrollbar]:hidden [-ms-overflow-style:none] [scrollbar-width:none]">
+          {resultValue === 0 && (
+            <div className="rounded-lg flex flex-col text-xl mt-5 w-full h-full">
+              <div className="p-3 w-full mb-5">
+                <p className="text-gray-900 text-sm rounded-xl p-8">
+                  {transcript}
+                </p>
+              </div>
+            </div>
+          )}
+
+          {resultValue === 1 && (
+            <div className="flex flex-col h-full w-full p-5 rounded-lg mt-2.5">
+              <div className="flex-1 overflow-y-auto border border-gray-200 rounded-md p-4">
+                {!loading ? (
+                  <div className="flex flex-col gap-4">
+                    {promptResponses?.map(
+                      (promptResponse, index) =>
+                        promptResponse && (
+                          <div
+                            key={index}
+                            className="p-3 border-b border-gray-200 last:border-b-0"
+                          >
+                            <ReactMarkdown>{promptResponse}</ReactMarkdown>
+                          </div>
+                        )
+                    )}
+                  </div>
+                ) : (
+                  <div className="flex flex-col items-center justify-center gap-5 text-center h-full w-full">
+                    <CircularProgress size={50} />
+                    <h1 className="text-xl">Analyzing Input</h1>
+                    <p className="text-sm">Generating response...</p>
+                  </div>
+                )}
+              </div>
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
